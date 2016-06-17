1b24b8ad64dc8a35908b60d4d04708cf931be811
Andrew McKittrick <amckit@ksu.edu>
2016-06-16 20:02:07 -0500

    Initial Commit

---
+++ HealtheBeacon/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  HealtheBeacon
+//
+//  Created by Andrew McKittrick on 6/16/16.
+//  Copyright © 2016 Andrew McKittrick. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
