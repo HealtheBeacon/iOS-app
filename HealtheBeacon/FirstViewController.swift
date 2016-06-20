7e0344ce3c6e702a777241a53e28ab16574b2402
Andrew McKittrick <amckit@ksu.edu>
2016-06-19 20:03:29 -0500

    first commit

--- HealtheBeacon/FirstViewController.swift
+++ HealtheBeacon/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  FirstViewController.swift
+//  SecondViewController.swift
 //  HealtheBeacon
 //
 //  Created by Andrew McKittrick on 6/16/16.
@@ -8,18 +8,56 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITextFieldDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
+    //MARK: Properties
+    
+    @IBOutlet var cernerIDTextField: UITextField!
+    @IBOutlet var nameTextField: UITextField!
+    var teams: [Int:String] = [1 : "HealtheIntent | Registires",
+                               2 : "HealtheIntent | Services",
+                               3 : "HealtheIntent | Infrastructure",
+                               4 : "Millennium | Development",
+                               5 : "test",
+                               6 : "another test"]
+    @IBOutlet var pickerView: UIPickerView!
+    let defaults = NSUserDefaults.standardUserDefaults()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.cernerIDTextField.delegate = self;
+        self.nameTextField.delegate = self;
+        pickerView.dataSource = self;
+        pickerView.delegate = self;
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        self.view.endEditing(true)
+        return false
+    }
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int{
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int{
+        return teams.count
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?{
+        defaults.setValue(row, forKey: "teamID")
+        return teams[row]
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func submit(sender: UIButton) {
+        defaults.setValue(cernerIDTextField.text, forKey: "cernerID")
+        defaults.setValue(nameTextField.text, forKey: "name")
+    }
 }
 
--- HealtheBeacon/SecondViewController.swift
+++ HealtheBeacon/SecondViewController.swift
@@ -1,25 +1,131 @@
 //
-//  SecondViewController.swift
-//  HealtheBeacon
+//  ViewController.swift
+//  Defining and Processing iBeacons - Source
 //
-//  Created by Andrew McKittrick on 6/16/16.
-//  Copyright © 2016 Andrew McKittrick. All rights reserved.
+//  Created by Vandad Nahavandipoor on 7/8/14.
+//  Copyright (c) 2014 Pixolity Ltd. All rights reserved.
 //
+//  These example codes are written for O'Reilly's iOS 8 Swift Programming Cookbook
+//  If you use these solutions in your apps, you can give attribution to
+//  Vandad Nahavandipoor for his work. Feel free to visit my blog
+//  at http://vandadnp.wordpress.com for daily tips and tricks in Swift
+//  and Objective-C and various other programming languages.
 
 import UIKit
+import CoreLocation
+import CoreBluetooth
 
-class SecondViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class SecondViewController: UIViewController, CBPeripheralManagerDelegate {
+    
+    var peripheralManager : CBPeripheralManager?
+    
+    /* A newly-generated UUID for our beacon */
+    let uuid = NSUUID()
+    
+    /* The identifier of our beacon is the identifier of our bundle here */
+    let identifier = NSBundle.mainBundle().bundleIdentifier!
+    let options = NSUserDefaults.standardUserDefaults()
+    
+    /* Made up major and minor versions of our beacon region */
+    let major: CLBeaconMajorValue = 1
+    let minor: CLBeaconMinorValue = 0
+    
+    func peripheralManagerDidStartAdvertising(peripheral: CBPeripheralManager,
+                                              error: NSError?){
+        
+        if error == nil{
+            print("Successfully started advertising our beacon data")
+            
+            let message = "Successfully set up your beacon. " +
+                "The unique identifier of our service is: \(uuid.UUIDString)"
+            
+            print(message)
+            
+            let controller = UIAlertController(title: "iBeacon",
+                                               message: message,
+                                               preferredStyle: .Alert)
+            
+            controller.addAction(UIAlertAction(title: "OK",
+                style: .Default,
+                handler: nil))
+            
+            presentViewController(controller, animated: true, completion: nil)
+            
+        } else {
+            print("Failed to advertise our beacon. Error = \(error)")
+        }
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager){
+        
+        peripheral.stopAdvertising()
+        
+        //print("The peripheral state is ", appendNewline: false)
+        switch peripheral.state{
+        case .PoweredOff:
+            print("Powered off")
+        case .PoweredOn:
+            print("Powered on")
+        case .Resetting:
+            print("Resetting")
+        case .Unauthorized:
+            print("Unauthorized")
+        case .Unknown:
+            print("Unknown")
+        case .Unsupported:
+            print("Unsupported")
+        }
+        
+        /* Bluetooth is now powered on */
+        if peripheral.state != .PoweredOn{
+            
+            let controller = UIAlertController(title: "Bluetooth",
+                                               message: "Please turn Bluetooth on",
+                                               preferredStyle: .Alert)
+            
+            controller.addAction(UIAlertAction(title: "OK",
+                style: .Default,
+                handler: nil))
+            
+            presentViewController(controller, animated: true, completion: nil)
+            
+        } else {
+            
+            let manufacturerData = identifier.dataUsingEncoding(
+                NSUTF8StringEncoding,
+                allowLossyConversion: false)
+            
+            let theUUid = CBUUID(NSUUID: uuid)
+            
+            let cernerID = options.stringForKey("cernerID")
+            
+            let teamID = options.stringForKey("teamID")
+            
+            let myInformation = cernerID! + " " + teamID!
+            
+            let dataToBeAdvertised:[String: AnyObject!] = [
+            CBAdvertisementDataLocalNameKey : myInformation,
+                CBAdvertisementDataManufacturerDataKey : manufacturerData,
+                CBAdvertisementDataServiceUUIDsKey : [theUUid],
+                ]
+            
+            peripheral.startAdvertising(dataToBeAdvertised)
+            
+        }
+        
     }
-
-
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)
+        peripheralManager = CBPeripheralManager(delegate: self, queue: queue)
+        if let manager = peripheralManager{
+            manager.delegate = self
+        }
+        
+    }
+    
+    
 }
-
